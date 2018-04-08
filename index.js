64dbaa74d10f996fca68ce033cac2b669d041b1a
icelord <36421376+xicelord@users.noreply.github.com>
2018-04-08 20:44:13 +0200

    Use sqlite3 instead of JSON; Remember wrong file-extensions

--- index.js
+++ index.js
@@ -1,84 +1,186 @@
 const fs = require('fs');
 const async = require('async');
 const child_process = require('child_process');
+const readChunk = require('read-chunk');
+const imageType = require('image-type');
+const sqliteDatabase = require('better-sqlite3');
 
+let path = (process.argv[2].endsWith('/') ? process.argv[2] : process.argv[2] + '/');
+let found = [];
 let counter = 0;
-let scan = {
-  total_changes: 0,
-  path: '',
-  processed: [],
-  found: []
-};
+let db;
 
-//Load scanfile if exists
+//Load database
 try {
-  scan = require('./scanfile.json');
-
-  try {
-    //Create a backup of the scanfile
-    fs.copyFileSync('./scanfile.json', './scanfile.json.bak' + (Date.now()));
-  } catch (ex) {
-    console.log(JSON.stringify(ex, null, 2));
-    console.log('Backup of scanfile failed');
-    return;
-  }
+  db = new sqliteDatabase('./scandb.sqlite3');
+  backupDb();
 } catch (ex) {
-  console.log('No readable scanfile found.');
+  console.error(ex);
+  throw new Error('Acessing the database-file failed.');
 }
 
+//Create the tables if necessary and fetch rows
+console.log('Preparing db & Fetching data from it')
+createTablesInDb();
+found = loadFoundFromDb();
+
 //Rescan if necessary
-if (scan.found.length === 0) {
-  console.log('Scanning directory "' + process.argv[2]  + '". This might take some time.');
-  scan.found = fs.readdirSync(process.argv[2]).filter((el) => {
-    return !scan.processed.includes(el);
+if (found.length === 0) {
+  //Fetch processed rows form db
+  let processed = loadProcessedFromDb();
+
+  console.log('Scanning directory "' + process.argv[2]  + '".');
+  found = fs.readdirSync(process.argv[2]).filter((el) => {
+    return !processed.includes(el);
   });
 
-  //TODO: FILTER ALREADY SCANNED!
-
   //Abort if no directories were found
-  if (scan.found.length === 0) {
+  if (found.length === 0) {
     console.log('No directories were found.');
     return;
   } else {
-    scan.path = (process.argv[2].endsWith('/') ? process.argv[2] : process.argv[2] + '/');
-    fs.writeFileSync('./scanfile.json', JSON.stringify(scan));
+    console.log('Inserting found directories into the database. This might take some time.');
+    let timestamp = Date.now();
+    insertFoundIntoDb(found);
+    console.log('Operation took ' + (Date.now() - timestamp) + 'ms');
   }
 } else {
   console.log('Resuming work on already queued directories');
 }
 
-console.log('Queue has ' + scan.found.length + ' entries');
-
-//Copy array & Start processing (Reverse because shift is way more expensive than pop)
-let found_copy = scan.found.slice().reverse();
-async.mapSeries(found_copy, processDirectory, (err) => {
+//Start processing
+console.log('Queue has ' + found.length + ' entries');
+async.mapSeries(found, processDirectory, (err) => {
   if (err) {
-    console.error(err);
-    return;
+    console.error(JSON.stringify(err, null, 2));
+    throw new Error('A fatal error occured');
   } else {
     console.log('All done :)');
-    console.log('Total changes: ' + (scan.total_changes >= 0 ? '+' : '') + scan.total_changes + ' b');
     return;
   }
 });
 
 
+//Function to create a backup of the db
+function backupDb() {
+  try {
+    fs.copyFileSync('./scandb.sqlite3', './scandb.sqlite3.bak' + (Date.now()));
+  } catch (ex) {
+    console.error(ex);
+    throw new Error('Backup of database failed');
+  }
+}
+
+//Function to create the tables if necessary
+function createTablesInDb(cb) {
+  //Create tables (if they don't exist)
+  try {
+    db.exec(
+      'CREATE TABLE IF NOT EXISTS found (directory VARCHAR(50)); ' +
+      'CREATE TABLE IF NOT EXISTS processed (directory VARCHAR(50), changes INTEGER); ' +
+      'CREATE TABLE IF NOT EXISTS wrong_extensions (directory VARCHAR(50), file VARCHAR(12), correct_fileext VARCHAR(4))',
+    );
+  } catch (ex) {
+    //Abort on error
+    console.error(ex);
+    throw new Error('Creating the tables failed.');
+  }
+}
+
+//Function to get found from the db
+function loadFoundFromDb() {
+  try {
+    //Load found directories from database & map the to better format
+    return db.prepare('SELECT * FROM found').all().map((row) => row.directory);
+  } catch (ex) {
+    //Abort on error
+    console.error(ex);
+    throw new Error('Fetching rows from the db failed.');
+  }
+}
+
+//Function to insert found into the db
+function insertFoundIntoDb(found) {
+  //Create chunks of 1k items
+  let chunks = createChunkedArray(found, 1000);
+  let finished = 0;
+
+  try {
+    //Insert the chunks
+    for (let i = 0; i < chunks.length; i++) {
+      var stmt = db.transaction(new Array(chunks[i].length).fill('INSERT INTO found VALUES (?)'));
+      stmt.run(chunks[i]);
+    }
+  } catch (ex) {
+    //Abort on error
+    console.error(ex);
+    throw new Error('Inserting the found directories failed');
+  }
+}
+
+//Function the check if directory was already processed
+function loadProcessedFromDb(cb) {
+  try {
+    //Load processed directories from database & map the to better format
+    return db.prepare('SELECT directory FROM processed').all().map((row) => row.directory);
+  } catch (ex) {
+    //Abort on error
+    console.error(ex);
+    throw new Error('Fetching rows from the db failed.');
+  }
+}
+
+//Function to add directory to processed and remove from found
+function flagProcessed(directory, changes, cb) {
+  try {
+    //Insert & Remove
+    db.prepare('DELETE FROM found WHERE directory = ?').run(directory);
+    db.prepare('INSERT INTO processed VALUES (?, ?)').run(directory, changes);
+  } catch (ex) {
+    console.error(ex);
+    throw new Error('Updating the database failed');
+  }
+}
+
+//Function to add files with wrong extension to database
+function reportWrongExtension(directory, file, realImageType) {
+  try {
+    //Insert
+    db.prepare('INSERT INTO wrong_extensions VALUES (?, ?, ?)').run(directory, file, (realImageType !== null ? realImageType.ext : '?'));
+  } catch (ex) {
+    console.error(ex);
+    throw new Error('Updating the database failed');
+  }
+}
+
 //Function responsible for processing a directory/chapter
 function processDirectory(directory, processDirectory_callback) {
-  console.log('Processing "' + directory + '" (Total changes: ' + (scan.total_changes >= 0 ? '+' : '') + scan.total_changes + ' b)');
+  console.log('Processing "' + directory + '"');
 
   //Scan folder for content
-  fs.readdir(scan.path + directory, (err, files) => {
+  fs.readdir(path + directory, (fs_err, fs_files) => {
+    //Abort if the directory-listing failed
+    if (fs_err) {
+      processDirectory_callback(fs_err);
+    }
+
     //Process PNGs
     async.mapLimit(
-      files,
+      fs_files,
       4,
       (file, asyncFilter_callback) => {
         //Check if is PNG
         if (file.toLowerCase().endsWith('.png')) {
+          //Abort if not really a PNG
+          let realImageType = imageType(readChunk.sync((path + directory + '/' + file), 0, 12));
+          if (!realImageType || realImageType.ext != 'png') {
+            reportWrongExtension(directory, file, realImageType);
+            return asyncFilter_callback(null, 0);
+          }
+
           //Send to pngout
           child_process.exec(
-            './pngout "' + (scan.path + directory + '/' + file) + '"',
+            './pngout "' + (path + directory + '/' + file) + '"',
             (error, stdout, stderr) => {
               //Abort on error; Ignore Error 2: Could not make image smaller
               if (error && error.code !== 2) {
@@ -104,26 +206,35 @@ function processDirectory(directory, processDirectory_callback) {
       (mapLimit_error, changes) => {
         //Update, Save & Notify
         if (!mapLimit_error) {
-          scan.processed.push(scan.found.pop());
+          //Calculate total changes in size
           let chapter_change = changes.reduce((a, b) => a+b, 0);
-          scan.total_changes += chapter_change;
-          fs.writeFileSync('./scanfile.json', JSON.stringify(scan));
+
+          //Notify db & user
+          flagProcessed(directory, chapter_change);
           console.log('Done! (' + (chapter_change >= 0 ? '+' : '') + chapter_change + ' b)');
-        }
 
-        //Make backup if necessary
-        counter++;
-        if (counter % 100 === 0) {
-          try {
-            fs.copyFileSync('./scanfile.json', './scanfile.json.bak' + (Date.now()));
-          } catch (ex) {
-            return processDirectory_callback(ex);
-          }
-        }
+          //Make backup if necessary
+          counter++;
+          if (counter % 100 === 0) { backupDb(); }
 
-        //Report to async
-        processDirectory_callback(mapLimit_error);
+          //Report to async
+          processDirectory_callback(null);
+        } else {
+          //Report error to async
+          processDirectory_callback(mapLimit_error);
+        }
       }
     );
   });
+}
+
+//Chunk array
+function createChunkedArray(arr, chunkSize) {
+    var chunks = [], i;
+
+    for (i = 0; i < arr.length; i += chunkSize) {
+        chunks.push(arr.slice(i, i + chunkSize));
+    }
+
+    return chunks;
 }
